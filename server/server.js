b0422fd223d6cb174cdb050e51490842858c8c22
akarsu35 <cumhurakarsu@gmail.com>
2026-01-10 22:24:25 +0300

    urls

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 import http from 'http'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
-// Ensure Prisma receives DATABASE_URL from process.env
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 const PORT = process.env.PORT || 4000
 
 function sendJSON(res, status, data) {
