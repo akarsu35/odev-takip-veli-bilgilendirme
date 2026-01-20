425c3983b8698de8bcc17d2b502761ae6c0a5ae4
akarsu35 <cumhurakarsu@gmail.com>
2026-01-20 15:47:38 +0300

    redeploy

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import 'dotenv/config'
 export default defineConfig({
   engine: 'classic',
   datasource: {
-    url: env('DIRECT_URL') || env('DATABASE_URL'),
+    url: process.env.DIRECT_URL || process.env.DATABASE_URL || '',
   },
 })
